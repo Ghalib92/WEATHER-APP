4d142aa6006c145c90f6827606494a30b2a31648
Ghalib92 <ghalibmohammad92@gmail.com>
2025-01-28 12:28:22 +0300

    Added while loop and measures in degrees

--- app.py
+++ app.py
@@ -2,18 +2,25 @@ import requests
 
 api_key = '0aa329f750d27fa0233822cb30e7e2e3'
 
-user_input = input("Enter city: ")
+while True:
+    user_input = input("Enter city: ")
 
-weather_data = requests.get(
-    f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&unitsimperial&APPID={api_key}"
-)
+    weather_data = requests.get(
+        f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&unitsimperial&APPID={api_key}"
+    )
 
-if weather_data.json()['cod']=='404': #status code that the API returns
-    print("City not found.")
-else:
- 
+    if weather_data.json()['cod']=='404': #status code that the API returns
+        print("City not found.")
+    
+    
+    else:
+        weather = weather_data.json()['weather'][0]['main']
+        temp =round(weather_data.json()['main']['temp'])
+        temp_degrees = round(temp - 273.15)
+        print(f"The weather in {user_input} is {weather}")
+        print(f"The temperature in {user_input} is {temp_degrees} degrees")
 
-    weather = weather_data.json()['weather'][0]['main']
-    temp =round(weather_data.json()['main']['temp'])
-    print(f"The weather in {user_input} is {weather}")
-    print(f"The temperature in {user_input} is {temp} fahreinhets")
+    
+        choice = input("Do you want to check another city? (yes/no): ")
+        if choice.lower() != 'yes':
+            break 
